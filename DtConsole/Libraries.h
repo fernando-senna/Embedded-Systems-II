157dae35b44071642267a8260b503faf7a28eafc
Fernando Do Nascimento <fernandorgarcia@mavs.uta.edu>
2016-04-27 22:35:25 -0500

    First Commit - Multiple Header Files

---
+++ DtConsole/Libraries.h
@@ -0,0 +1,30 @@
+/*
+ *	Libraries.h - Libraries needed to operate the DT9816, set up the server using windows
+ *	sockets, and other functions needed to successfully run the project.
+ *	Authors: 	Fernando Do Nascimento
+ *			 	Isaac Ramirez
+ *	Class: 	 	CSE 4342 - Embedded Systems II
+ *	Lab:	 	Design of a Real-Data Acquisition System with Network Communications
+ *	Due Date:	May 5, 2016	
+ */
+#ifndef LIBRARIES_H
+#define LIBRARIES_H
+
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include "oldaapi.h"
+#include <fstream>
+#include <iostream>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <time.h>
+#include <vector>
+#include <WinSock.h>
+
+using namespace std;
+
+#endif
